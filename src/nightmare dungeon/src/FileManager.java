373e6c150daf67eaf8b4d23e28d8320633945472
hsarpaydemir <sarpisko123@gmail.com>
2017-12-13 21:56:24 +0300

    Began working on change character, solved the starting with music off problem, created a choose character menu.

--- src/nightmare dungeon/src/FileManager.java
+++ src/nightmare dungeon/src/FileManager.java
@@ -1,3 +1,40 @@
-public class FileManager {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.BufferedReader;
 
+public class FileManager {
+    public String[] highScores = new String[10];
+    BufferedReader br = null;
+    FileReader fr = null;
+
+    public FileManager(String filename){
+        readFromFile(filename);
+    }
+
+    public void readFromFile(String filename){
+        try {
+            fr = new FileReader(filename);
+            br = new BufferedReader(fr);
+            String currentLine;
+            int i = 0;
+            while ((currentLine = br.readLine()) != null) {
+                highScores[i] = currentLine;
+                i++;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (br != null)
+                    br.close();
+                if (fr != null)
+                    fr.close();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
 }
--- src/nightmare dungeon/src/HighScores.java
+++ src/nightmare dungeon/src/HighScores.java
@@ -9,10 +9,10 @@ import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 
 public class HighScores extends BasicGameState{
-	
+	FileManager fm;
 	//constructor
 	public HighScores(int highScores){
-		
+		fm = new FileManager("src/nightmare dungeon/res/highScores.txt");
 	}
 	
 	public void init(GameContainer gc, StateBasedGame sbg) throws SlickException {
@@ -23,6 +23,16 @@ public class HighScores extends BasicGameState{
 	public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException {
 		//images are drawn
 		g.drawImage(new Image("src/nightmare dungeon/res/highScore.png"), 250, 100);
+		g.drawString(fm.highScores[0], 250, 200);
+		g.drawString(fm.highScores[1], 250, 250);
+		g.drawString(fm.highScores[2], 250, 300);
+		g.drawString(fm.highScores[3], 250, 350);
+		g.drawString(fm.highScores[4], 250, 400);
+		g.drawString(fm.highScores[5], 250, 450);
+		g.drawString(fm.highScores[6], 250, 500);
+		g.drawString(fm.highScores[7], 250, 550);
+		g.drawString(fm.highScores[8], 250, 600);
+		g.drawString(fm.highScores[9], 250, 650);
 		g.drawString("Press Esc to go back", 50, 800);
 	}
 
